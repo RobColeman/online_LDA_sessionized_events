558a81408f2f146c1b576fb96ade6db139d64ef2
RobColeman <rob@preact.com>
2015-10-12 18:11:39 -0700

    first commit

---
+++ onlineldavb.py
@@ -0,0 +1,284 @@
+# onlineldavb.py: Package of functions for fitting Latent Dirichlet
+# Allocation (LDA) with online variational Bayes (VB).
+#
+# Copyright (C) 2010  Matthew D. Hoffman
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+import sys, re, time, string
+import numpy as n
+from scipy.special import gammaln, psi
+
+n.random.seed(100000001)
+meanchangethresh = 0.001
+
+def dirichlet_expectation(alpha):
+    """
+    For a vector theta ~ Dir(alpha), computes E[log(theta)] given alpha.
+    """
+    if (len(alpha.shape) == 1):
+        return(psi(alpha) - psi(n.sum(alpha)))
+    return(psi(alpha) - psi(n.sum(alpha, 1))[:, n.newaxis])
+
+def parse_doc_list(sessions, vocab):
+    """
+    Arguments: 
+    sessions:  List of D sessions.
+    vocab: Dictionary mapping from eventIds to integer ids.
+
+    Returns a pair of lists of lists. 
+
+    The first, wordids, says what vocabulary tokens are present in
+    each document. wordids[i][j] gives the jth unique token present in
+    document i. (Don't count on these tokens being in any particular
+    order.)
+
+    The second, wordcts, says how many times each vocabulary token is
+    present. wordcts[i][j] is the number of times that the token given
+    by wordids[i][j] appears in document i.
+    """
+    if (type(sessions).__name__ == 'str'):
+        sessions = [sessions]
+
+    D = len(sessions)
+    
+    wordids = list()
+    wordcts = list()
+    for D in range(0, D):
+        events = sessions[D]
+        ddict = dict()
+        for e in events:
+            if (e in vocab):
+                eventtoken = vocab[e]
+                if (not eventtoken in ddict):
+                    ddict[eventtoken] = 0
+                ddict[eventtoken] += 1
+        wordids.append(ddict.keys())
+        wordcts.append(ddict.values())
+
+    return((wordids, wordcts))
+
+class OnlineLDA:
+    """
+    Implements online VB for LDA as described in (Hoffman et al. 2010).
+    """
+
+    def __init__(self, vocab, K, D, alpha = None, eta = None, tau0 = 1024, kappa = 0.7):
+        """
+        Arguments:
+        K: Number of topics
+        vocab: A set of words to recognize. When analyzing documents, any word
+           not in this set will be ignored.
+        D: Total number of documents in the population. For a fixed corpus,
+           this is the size of the corpus. In the truly online setting, this
+           can be an estimate of the maximum number of documents that
+           could ever be seen.
+        alpha: Hyperparameter for prior on weight vectors theta
+        eta: Hyperparameter for prior on topics beta
+        tau0: A (positive) learning parameter that downweights early iterations
+        kappa: Learning rate: exponential decay rate---should be between
+             (0.5, 1.0] to guarantee asymptotic convergence.
+
+        Note that if you pass the same set of D documents in every time and
+        set kappa=0 this class can also be used to do batch VB.
+        """
+        self._vocab = dict()
+        for word in vocab:
+            word = word.lower()
+            self._vocab[word] = len(self._vocab)
+
+        self._K = K
+        self._W = len(self._vocab)
+        self._D = D
+        self._alpha = alpha if alpha else 1.0 / K
+        self._eta = eta if eta else 1.0 / K
+        self._tau0 = tau0 + 1
+        self._kappa = kappa
+        self._updatect = 0
+
+        # Initialize the variational distribution q(beta|lambda)
+        self._lambda = 1 * n.random.gamma(100.0, 1.0 / 100.0, (self._K, self._W))
+        self._Elogbeta = dirichlet_expectation(self._lambda)
+        self._expElogbeta = n.exp(self._Elogbeta)
+
+    def do_e_step(self, sessions):
+        """
+        
+        Given a mini-batch of documents, estimates the parameters
+        gamma controlling the variational distribution over the topic
+        weights for each document in the mini-batch.
+
+        Arguments:
+        sessions:  List of D sessions.
+
+        Returns a tuple containing the estimated values of gamma,
+        as well as sufficient statistics needed to update lambda.
+
+        """
+        # This is to handle the case where someone just hands us a single
+        # document, not in a list.
+        if (type(sessions).__name__ == 'string'):
+            temp = list()
+            temp.append(sessions)
+            sessions = temp
+
+        (wordids, wordcts) = parse_doc_list(sessions, self._vocab)
+        batchD = len(sessions)
+
+        # Initialize the variational distribution q(theta|gamma) for
+        # the mini-batch
+        gamma = 1*n.random.gamma(100., 1./100., (batchD, self._K))
+        Elogtheta = dirichlet_expectation(gamma)
+        expElogtheta = n.exp(Elogtheta)
+
+        sstats = n.zeros(self._lambda.shape)
+        # Now, for each document d update that document's gamma and phi
+        it = 0
+        meanchange = 0
+        for d in range(0, batchD):
+            # These are mostly just shorthand (but might help cache locality)
+            ids = wordids[d]
+            cts = wordcts[d]
+            gammad = gamma[d, :]
+            Elogthetad = Elogtheta[d, :]
+            expElogthetad = expElogtheta[d, :]
+            expElogbetad = self._expElogbeta[:, ids]
+            # The optimal phi_{dwk} is proportional to 
+            # expElogthetad_k * expElogbetad_w. phinorm is the normalizer.
+            phinorm = n.dot(expElogthetad, expElogbetad) + 1e-100
+            # Iterate between gamma and phi until convergence
+            for it in range(0, 100):
+                lastgamma = gammad
+                # We represent phi implicitly to save memory and time.
+                # Substituting the value of the optimal phi back into
+                # the update for gamma gives this update. Cf. Lee&Seung 2001.
+                gammad = self._alpha + expElogthetad * \
+                    n.dot(cts / phinorm, expElogbetad.T)
+                Elogthetad = dirichlet_expectation(gammad)
+                expElogthetad = n.exp(Elogthetad)
+                phinorm = n.dot(expElogthetad, expElogbetad) + 1e-100
+                # If gamma hasn't changed much, we're done.
+                meanchange = n.mean(abs(gammad - lastgamma))
+                if (meanchange < meanchangethresh):
+                    break
+            gamma[d, :] = gammad
+            # Contribution of document d to the expected sufficient
+            # statistics for the M step.
+            sstats[:, ids] += n.outer(expElogthetad.T, cts/phinorm)
+
+        # This step finishes computing the sufficient statistics for the
+        # M step, so that
+        # sstats[k, w] = \sum_d n_{dw} * phi_{dwk} 
+        # = \sum_d n_{dw} * exp{Elogtheta_{dk} + Elogbeta_{kw}} / phinorm_{dw}.
+        sstats = sstats * self._expElogbeta
+
+        return((gamma, sstats))
+
+    def update_lambda(self, sessions):
+        """
+        First does an E step on the mini-batch given in wordids and
+        wordcts, then uses the result of that E step to update the
+        variational parameter matrix lambda.
+
+        Arguments:
+        sessions:  List of D documents. Each document must be represented
+               as a string. (Word order is unimportant.) Any
+               words not in the vocabulary will be ignored.
+
+        Returns gamma, the parameters to the variational distribution
+        over the topic weights theta for the documents analyzed in this
+        update.
+
+        Also returns an estimate of the variational bound for the
+        entire corpus for the OLD setting of lambda based on the
+        documents passed in. This can be used as a (possibly very
+        noisy) estimate of held-out likelihood.
+        """
+
+        # rhot will be between 0 and 1, and says how much to weight
+        # the information we got from this mini-batch.
+        rhot = pow(self._tau0 + self._updatect, -self._kappa)
+        self._rhot = rhot
+        # Do an E step to update gamma, phi | lambda for this
+        # mini-batch. This also returns the information about phi that
+        # we need to update lambda.
+        (gamma, sstats) = self.do_e_step(sessions)
+        # Estimate held-out likelihood for current values of lambda.
+        bound = self.approx_bound(sessions, gamma)
+        # Update lambda based on documents.
+        self._lambda = self._lambda * (1-rhot) + \
+            rhot * (self._eta + self._D * sstats / len(sessions))
+        self._Elogbeta = dirichlet_expectation(self._lambda)
+        self._expElogbeta = n.exp(self._Elogbeta)
+        self._updatect += 1
+
+        return(gamma, bound)
+
+    def approx_bound(self, sessions, gamma):
+        """
+        Estimates the variational bound over *all documents* using only
+        the documents passed in as "sessions." gamma is the set of parameters
+        to the variational distribution q(theta) corresponding to the
+        set of documents passed in.
+
+        The output of this function is going to be noisy, but can be
+        useful for assessing convergence.
+        """
+
+        # This is to handle the case where someone just hands us a single
+        # document, not in a list.
+        if (type(sessions).__name__ == 'string'):
+            temp = list()
+            temp.append(sessions)
+            sessions = temp
+
+        (wordids, wordcts) = parse_doc_list(sessions, self._vocab)
+        batchD = len(sessions)
+
+        score = 0
+        Elogtheta = dirichlet_expectation(gamma)
+        expElogtheta = n.exp(Elogtheta)
+
+        # E[log p(sessions | theta, beta)]
+        for d in range(0, batchD):
+            gammad = gamma[d, :]
+            ids = wordids[d]
+            cts = n.array(wordcts[d])
+            phinorm = n.zeros(len(ids))
+            for i in range(0, len(ids)):
+                temp = Elogtheta[d, :] + self._Elogbeta[:, ids[i]]
+                tmax = max(temp)
+                phinorm[i] = n.log(sum(n.exp(temp - tmax))) + tmax
+            score += n.sum(cts * phinorm)
+#             oldphinorm = phinorm
+#             phinorm = n.dot(expElogtheta[d, :], self._expElogbeta[:, ids])
+#             print oldphinorm
+#             print n.log(phinorm)
+#             score += n.sum(cts * n.log(phinorm))
+
+        # E[log p(theta | alpha) - log q(theta | gamma)]
+        score += n.sum((self._alpha - gamma)*Elogtheta)
+        score += n.sum(gammaln(gamma) - gammaln(self._alpha))
+        score += sum(gammaln(self._alpha*self._K) - gammaln(n.sum(gamma, 1)))
+
+        # Compensate for the subsampling of the population of documents
+        score = score * self._D / len(sessions)
+
+        # E[log p(beta | eta) - log q (beta | lambda)]
+        score = score + n.sum((self._eta-self._lambda)*self._Elogbeta)
+        score = score + n.sum(gammaln(self._lambda) - gammaln(self._eta))
+        score = score + n.sum(gammaln(self._eta*self._W) - 
+                              gammaln(n.sum(self._lambda, 1)))
+
+        return(score)
